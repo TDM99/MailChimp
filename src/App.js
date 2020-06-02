fd8fadd5bded7d9cab510553db15d8e82a6b173b
Teison Montero <teisonmontero99@gmail.com>
2020-06-02 11:39:55 -0400

    add readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { FormLogin } from './components/FormLogin';
-import { Loading } from './components/Loading';
-import { Success } from './components/Success';
 
 function App() {
   return (
     <div>
-      <Success />
-      {/* <Loading /> */}
-      {/* <FormLogin /> */}
+      <FormLogin />
     </div>
   );
 }
